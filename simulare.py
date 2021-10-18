bcb824a38ac1aa8ee69d6844041d1e20e7037c7f
LewluNotBOT <66873306+Lewluu@users.noreply.github.com>
2021-10-18 13:41:14 +0300

    da

--- simulare.py
+++ simulare.py
@@ -6,9 +6,18 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.animation import Animation
 from kivy.uix.button import Button
+from kivy.graphics import Rectangle
 
 class Sim(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        with self.canvas:
+             self.rect=Rectangle(pos=(0,0),size=(100,100))
+
+        anim=Animation(pos=(100,100))
+        anim.start(Widget)
+
 
 class SimApp(App):
     def build(self):
