d0c1cba41967f467fd4f8c9f660e5d6b6f47e486
LewluNotBOT <66873306+Lewluu@users.noreply.github.com>
2021-10-20 18:34:07 +0300

    Update simulare.py

--- simulare.py
+++ simulare.py
@@ -1,11 +1,9 @@
 import pyglet
 from pyglet import shapes
-from pyglet.window import key
+from pyglet import clock
 
 window=pyglet.window.Window(1200,800)
 batch=pyglet.graphics.Batch()
-keys=key.KeyStateHandler()
-window.push_handlers(keys)
 
 rectangle=shapes.Rectangle(80,80,150,150,color=(125,0,75),batch=batch)
 
@@ -14,11 +12,31 @@ def on_key_press(symbol,modifies):
     ikey=chr(symbol)
     if ikey=='a':
         rectangle.x-=5
+    if ikey=='d':
+        rectangle.x+=5
+
+
+
+@window.event
+def on_key_press(symbol,modifies):
+    MOVE_RIGHT=False
+    ikey=chr(symbol)
+    if ikey=='a':
+        rectangle.x-=5
+    if ikey=='d':
+        MOVE_RIGHT=True
+
+def callback(dt):
+    if MOVE_RIGHT:
+        rectangle.x+=5
+
+clock.schedule_interval(callback,1)
+
 
 @window.event
 def on_draw():
-    print("Drawing!!!")
     window.clear()
+    callback("Call back !!!")
     batch.draw()
 
 pyglet.app.run()
