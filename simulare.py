fa647d665765ab7aab9998a7362a72c7b30ccc79
LewluNotBOT <66873306+Lewluu@users.noreply.github.com>
2021-10-24 13:40:47 +0300

    da

--- simulare.py
+++ simulare.py
@@ -9,47 +9,14 @@ window=pyglet.window.Window(1200,800)
 window.set_location(350,150)
 batch=pyglet.graphics.Batch()
 
-rectangle=shapes.Rectangle(280,280,100,100,color=(125,0,75),batch=batch)
+rectangle=shapes.Rectangle(125,125,100,100,color=(125,0,75),batch=batch)
 rectangle.anchor_position=(50,50)
 
-speed=5
-mvright=mvleft=mvup=mvdown=False
-
-@window.event
-def on_key_press(symbol,modifies):
-    global mvright, mvleft,mvup,mvdown
-    ikey=chr(symbol)
-    if ikey=='a':
-        mvleft=True
-    if ikey=='d':
-        mvright=True
-    if ikey=='w':
-        mvup=True
-    if ikey=='s':
-        mvdown=True
-
-@window.event
-def on_key_release(symbol,modifies):
-    global mvright, mvleft,mvup,mvdown
-    ikey=chr(symbol)
-    if ikey=='a' and mvleft==True:
-        mvleft=False
-    if ikey=='d' and mvright==True:
-        mvright=False
-    if ikey=='w' and mvup==True:
-        mvup=False
-    if ikey=='s' and mvdown==True:
-        mvdown=False
+speed=2.5
 
 def MainLoop(dt):
-    if mvright:
-        rectangle.x+=speed
-    if mvleft:
-        rectangle.x-=speed
-    if mvup:
+    if rectangle.y < 750:
         rectangle.y+=speed
-    if mvdown:
-        rectangle.y-=speed
 
 
 clock.schedule_interval(MainLoop,0.0005)
