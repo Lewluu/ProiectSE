08ec221c2cb8fe80907788c6a819f1d67505fbc7
LewluNotBOT <66873306+Lewluu@users.noreply.github.com>
2021-10-24 13:37:28 +0300

    da

--- simulare.py
+++ simulare.py
@@ -1,13 +1,16 @@
 import pyglet
 from pyglet import shapes
 from pyglet import clock
+import random
+
+from pyglet import gl
 
 window=pyglet.window.Window(1200,800)
 window.set_location(350,150)
 batch=pyglet.graphics.Batch()
 
 rectangle=shapes.Rectangle(280,280,100,100,color=(125,0,75),batch=batch)
-rectangle.anchor_position=(25,25)
+rectangle.anchor_position=(50,50)
 
 speed=5
 mvright=mvleft=mvup=mvdown=False
@@ -38,7 +41,6 @@ def on_key_release(symbol,modifies):
     if ikey=='s' and mvdown==True:
         mvdown=False
 
-
 def MainLoop(dt):
     if mvright:
         rectangle.x+=speed
@@ -52,11 +54,28 @@ def MainLoop(dt):
 
 clock.schedule_interval(MainLoop,0.0005)
 
+#polygon
+points_list=[
+    [50,50,50,750],
+    [50,750,1150,750],
+    [1150,750,1150,50],
+    [1150,50,50,50]]
+
+polygon=pyglet.graphics.Batch()
+
+for points in points_list:
+    polygon.add(2,pyglet.gl.GL_LINES,None,
+                ('v2i',points),
+                ('c3B',(255,0,0,255,0,0)))
+
 
 @window.event
 def on_draw():
     window.clear()
+    #drawing the batch included graphics (ex:the rectangle)
     batch.draw()
+    #drawing the polygon
+    polygon.draw()
 
 pyglet.app.run()
 
